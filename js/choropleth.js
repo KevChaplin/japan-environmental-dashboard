3eea24702d9e9529d5bc0552f363bb6201471c8c
KevChaplin <kevchaplin44@gmail.com>
2021-10-07 12:47:58 +0900

    temerature data linked with prefecture data and displayed on map with color scale
    also combined fetch functions due to async data retrieval

--- js/choropleth.js
+++ js/choropleth.js
@@ -3,6 +3,18 @@ const h= 700
 
 const japanMapDataUrl = "https://raw.githubusercontent.com/deldersveld/topojson/master/countries/japan/jp-prefectures.json"
 
+const dataBaseUrl = "https://dashboard.e-stat.go.jp/api/1.0/Json/getData?"
+
+let temperatureData = []
+// Data: Average Air Temperature / Year / Prefecture
+let indicatorId = "0102010000000010010"
+let dataUrl = `${dataBaseUrl}&IndicatorCode=${indicatorId}`
+
+
+// Colors for representing temperature differential (z-axis). Array of colors from d3-scale-chromatic. Color scale reversed to match data.
+const colors = []
+d3.schemeRdYlBu[9].forEach(color => colors.unshift(color))
+
 const svgMap = d3.select("body")
                     .append("svg")
                     .attr("id", "map-canvas")
@@ -20,23 +32,59 @@ const geoGenerator = d3.geoPath()
                     .projection(projection)
 
 const drawMap = () => {
+  let year = 1975
 
+  // Colors representing temperature variance
+  const colorScale = d3.scaleQuantile()
+                  .domain(d3.extent(temperatureData, d => d.value))
+                  .range(colors)
+
+  // Creat map using d3.geoPath on converted topojson data
   svgMap.selectAll("path")
           .data(prefectureData)
           .enter()
           .append("path")
           .attr("d", geoGenerator)
           .attr("class", "prefecture")
+          // Prefecture name used to match and obtain temperature data.
+          .attr("fill", prefDataItem => {
+            let id = prefDataItem.properties.NAME_1
+            let pref = temperatureData.find(item => {
+              return ( (item.year === year) && (item.prefecture === id) )
+            })
+            let avTemperature = pref.value
+            return colorScale(avTemperature)
+          })
 }
 
+// Import data - map data is converted from topojson format; temperature data imported after map data - see fetchData.js
 d3.json(japanMapDataUrl).then(
   (data, error) => {
     if(error) {
       console.log(error)
     } else {
       prefectureData = topojson.feature(data, data.objects.JPN_adm1).features
-      getTemperatureData()
-      drawMap()
+      // fix error in data "Naosaki" should be "Nagasaki" - below is temporary fix, issue raised on data repository 2021/10/07
+      prefectureData[26].properties["NAME_1"] = "Nagasaki"
+      // Once map data is obtained, fetch climate data
+      d3.json(dataUrl).then(
+        (data, error) => {
+          if(error) {
+            console.log(error)
+          } else {
+            let dataAll =data.GET_STATS.STATISTICAL_DATA.DATA_INF.DATA_OBJ
+            temperatureData = dataAll.map(item => {
+              return {
+                    year: parseInt(item.VALUE["@time"].substring(0,4)),
+                    prefecture: prefectures[parseInt(item.VALUE["@regionCode"].substring(0,2)) -1],
+                    value: parseFloat(item.VALUE["$"])
+              }
+            })
+            // Once map data and climate data obtained, can draw map
+            drawMap()
+          }
+        }
+      )
     }
   }
 )
